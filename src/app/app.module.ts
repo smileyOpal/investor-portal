84af24a8114a44b41cbb392b143fc7a8c8d9f935
opal.cha <opal.cha@ascendcorp.com>
2021-06-06 11:35:07 +0700

    generate fund-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { FundListComponent } from './fund-list/fund-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    FundListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/fund-list/fund-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FundListComponent } from './fund-list.component';
+
+describe('FundListComponent', () => {
+  let component: FundListComponent;
+  let fixture: ComponentFixture<FundListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FundListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FundListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
