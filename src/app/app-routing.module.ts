a3ad956f4c297fe000a984dfee378e89407b7cd8
opal.cha <opal.cha@ascendcorp.com>
2021-06-06 11:56:55 +0700

    add navigate from login to funds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {FundListComponent} from './fund-list/fund-list.component';
+import {PageNotfoundComponent} from "./page-notfound/page-notfound.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'funds', component: FundListComponent},
+  {path: '**', component: PageNotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { FundListComponent } from './fund-list/fund-list.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {FundListComponent} from './fund-list/fund-list.component';
 import {FormsModule} from "@angular/forms";
+import {PageNotfoundComponent} from './page-notfound/page-notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FundListComponent
+    FundListComponent,
+    PageNotfoundComponent
   ],
   imports: [
     BrowserModule,
@@ -21,4 +23,5 @@ import {FormsModule} from "@angular/forms";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
