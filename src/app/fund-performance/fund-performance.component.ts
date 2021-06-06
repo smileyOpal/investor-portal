8fc20762523e4f6418672e09fb04c1f6b55dbd91
opal.cha <opal.cha@ascendcorp.com>
2021-06-07 00:46:26 +0700

    adjust dougnut chart

--- src/app/fund-performance/fund-performance.component.ts
+++ src/app/fund-performance/fund-performance.component.ts
@@ -13,40 +13,35 @@ export class FundPerformanceComponent implements OnInit {
   investorId: string | undefined;
   investor: Investor | undefined;
   fundDetail: FundDetail | undefined;
-  dealAllocationDataChart = {
+  dealAllocationChartData = {
     labels: ['Energy', 'Consumer Durables & Apparel', 'Customer Services', 'Insurance', 'Material', 'Software & Services', 'Technology Hardware & Equipment', 'Health Care Equipment & Services'],
     datasets: [
       {
         label: "My Stats Chart",
         data: [1, 1, 1, 1, 1, 1, 1, 1],
-        backgroundColor: ['#1abc9c', '#3498db', '#9b59b6', '#bdc3c7', '#f39c12', '#d527db', '#d527db', '#db8127']
+        backgroundColor: ['#1abc9c', '#3498db', '#9b59b6', '#bdc3c7', '#25dbd5', '#d527db', '#d8db25', '#db8127']
       }
     ]
   };
+  dealAllocationChartOptions = {
+    responsive: true
+  };
   investmentChartOptions = {
-    scales: {
-      y: {
-        suggestedMin: 0,
-        suggestedMax: 400
-      }
-    }
   }
   investmentChartData = {
     labels: ['2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024', '2025', '2026'],
     datasets: [
       {
         label: "Cumulative investment cost",
-        data: [0, 0, 50, 70, 210, 300, 300],
+        data: [0, 0, 50, 65, 210, 300, 300],
         fill: false,
-        borderColor: ['#1abc9c'],
-        tension: 0.1
+        borderColor: ['#1abc9c']
       },
       {
         label: "Cumulative total value",
-        data: [0, 0, 50, 70, 210, 230, 230],
+        data: [0, 0, 50, 70, 215, 230, 230],
         fill: false,
-        borderColor: ['#db8127'],
-        tension: 0.1
+        borderColor: ['#db8127']
       }
     ]
   }
